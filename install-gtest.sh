46d929a9d47a2b5b687d24c7281980c15dad7265
goforbroke <go.for.broke1006@gmail.com>
2021-12-01 21:40:29 +0300

    add gtest and test for List class

--- install-gtest.sh
+++ install-gtest.sh
@@ -4,10 +4,12 @@ set -e
 
 sudo apt-get install -y libgtest-dev
 
-sudo apt-get install -y cmake # install cmake
-cd /usr/src/gtest || exit 1
-sudo cmake CMakeLists.txt
-sudo make
-sudo cp ./*.a /usr/lib
-sudo cp ./lib/*.a /usr/lib
-sudo ldconfig
+sudo apt-get install -y cmake
+(
+  cd /usr/src/gtest || exit 1
+  sudo cmake CMakeLists.txt
+  sudo make
+  sudo cp ./*.a /usr/lib
+  sudo cp ./lib/*.a /usr/lib
+  sudo ldconfig
+)

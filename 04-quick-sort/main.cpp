63c96404f3b46c0679b91e6bc527af8a9dc22803
goforbroke <go.for.broke1006@gmail.com>
2022-03-21 05:39:55 +0300

    fix build

--- 04-quick-sort/main.cpp
+++ 04-quick-sort/main.cpp
@@ -2,3 +2,27 @@
 // Created by goforbroke on 21.03.2022.
 //
 
+#include <iostream>
+
+#include "solution.h"
+
+int main() {
+    std::cout << "Enter array length: ";
+    int length;
+    std::cin >> length;
+
+    std::cout << "Enter array elements: ";
+    int *arr = new int[length];
+    for (int i = 0; i < length; ++i) {
+        std::cin >> arr[i];
+    }
+
+    quick_sort(arr, 0, length - 1);
+
+    for (int i = 0; i < length; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
-build:
-	cmake -S . -B ./cmake-build-debug
+BUILD_DIR=./cmake-build-debug
 
-test: build
-	./cmake-build-debug/tests/learn-cpp-tests
+.PHONY: build test
+
+all: build test
+
+clear:
+	rm -rf ${BUILD_DIR}
+
+build:
+	cmake -S . -B "${BUILD_DIR}"
+	cd "${BUILD_DIR}" && make all
+
+test:
+	"${BUILD_DIR}/tests/learn-cpp-tests"
